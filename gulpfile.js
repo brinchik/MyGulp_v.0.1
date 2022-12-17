0627dc1bf3b9a95f7397568f3097a1c565c14575
brinchik <brunevich.work@gmail.com>
2022-12-18 00:53:31 +0300

    sass work. SLEEP

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
-const sass = require('sass');
+const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 const paths = {
     styles: {
@@ -8,7 +10,7 @@ const paths = {
         dest: 'dist/style/'
     },
     scripts: {
-        scr: 'src/scripts/**/*.js',
+        src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
     }
 }
@@ -17,4 +19,17 @@ function clean() {
     return del(['dist/*']);
 }
 
+function styles() {
+    return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(cleanCSS())
+    .pipe(rename({
+        basename: 'style',
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
 exports.clean = clean;
+exports.styles = styles;
